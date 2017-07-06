88f6a7cb6a9d457fb378941b1090a5f7c8df4463
szcf-weiya <2215235182@qq.com>
2017-07-06 18:13:20 +0800

    from win

--- Note2/note.R
+++ Note2/note.R
@@ -23,3 +23,56 @@ par(mfrow=c(1,1)); detach(fossum)
 ## The stem-and-leaf display
 with(ais, stem(ht[sport=="Row"]))
 
+## Boxplots
+## Base graphics boxplot function
+with(fossum, boxplot(totlngth, horiz=TRUE))
+## Alternative: lattice graphics bwplot function
+bwplot(~totlngth, data=fossum)
+
+
+##  Patterns in univariate time series
+## Panel A
+plot(log10(measles), xlab="", ylim=log10 (c(1,5000*1000)),
+     ylab=" Deaths; Population (log scale)", yaxt="n")
+ytiks <- c(1, 10, 100, 1000, 1000000, 5000000)
+## London population in thousands
+londonpop <-
+  ts(c(1088,1258,1504,1778,2073,2491,2921,3336,3881,4266,
+       4563,4541,4498,4408), start=1801, end=1931, deltat=10)
+points(log10(londonpop*1000), pch=16, cex=.5)
+axis(2, at=log10(ytiks), labels=paste(ytiks), las=2)
+## Panel B
+plot(window(measles, start=1840, end=1882), ylim=c (0, 4600),
+     yaxt="n")
+axis(2, at=(0:4)* 1000, labels=paste(0:4), las=2)
+
+## Panel A:
+par(fig=c(0, 1, .38, 1)) # 38% to 100% of page, in y-direction
+plot(log10(measles), ylab="log10(Deaths)",
+     ylim=log10(c(1,5000*1000)))
+mtext(side=3, line=0.5, "A (1629-1939)", adj=0)
+## Panel B: window from 1840 to 1882; more complete code
+par(fig=c(0, 1, 0, .4), new=TRUE) # 0% to 38% of height of figure region
+plot(window(measles, start=1840, end=1882), ylab="Deaths")
+mtext(side=3, line=0.5, "B (1841-1881)", adj=0)
+par(fig=c(0, 1, 0, 1)) # Restore default figure region
+
+
+## Patterns in bivariate data
+
+## Plot four vs one: data frame milk (DAAG)
+xyrange <- range(milk)
+plot(four ~ one, data = milk, xlim = xyrange, ylim = xyrange,
+     pch = 16, pty="s") # pty="s": square plotting region
+
+rug(milk$one) # x-axis rug (default is side=1)
+rug(milk$four, side = 2) # y-axis rug
+abline(0, 1)
+
+## The fitting of a smooth trend curve
+## Plot ohms vs juice: data frame fruitohms (DAAG)
+plot(ohms ~ juice, xlab="Apparent juice content (%)",
+     ylab="Resistance (ohms)", data=fruitohms)
+## Add a smooth curve, as in Panel B
+with(fruitohms, lines(lowess(juice, ohms), lwd=2))
+# With lwd=2, the curve is twice the default thickness
