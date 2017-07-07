3920e74429b2bb12bb0e4c8f233eaffa16019708
szcf-weiya <2215235182@qq.com>
2017-07-07 09:37:20 +0800

    update

--- lattice/ex.R
+++ lattice/ex.R
@@ -8,3 +8,10 @@ densityplot(~mpg, main = "Density Plot", xlab = "Miles per Gallon")
 densityplot(~mpg | cyl, main = "Density Plot", xlab = "Miles per Gallon")
 bwplot(cyl ~ mpg | gear, main = "Box plots by cylinders and gears", xlab = "Miles per Gallon", ylab = "Cylinders")
 
+xyplot(mpg ~ wt | cyl*gear, main = "Scatter Plots by Cylinders and Gears", xlab = "Car Weight", ylab = "Miles per Gallon")
+
+cloud(mpg ~ wt*qsec | cyl, main = "3D Scatter Plots by Cylinders")
+dotplot(cyl ~ mpg | gear, main = "Dot Plots by Number of Gears and Cylinders", xlab = "Miles Per Gallon")
+
+splom(mtcars[c(1,3,4,5,6)], main = "Scatter Plot Matrix for mtcars Data")
+detach(mtcars)
